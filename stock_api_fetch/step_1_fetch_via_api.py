eac66ba10201bdb5e69f4b833cee8303a7d8cbeb
Mehmet Akif Kucuk <mehmetakifkucuk@gmail.com>
2023-05-29 16:59:53 +0400

    .

--- stock_api_fetch/step_1_fetch_via_api.py
+++ stock_api_fetch/step_1_fetch_via_api.py
@@ -1,10 +1,33 @@
+# import necessary libraries
+import pandas as pd
+import numpy as np
 import requests
 
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
-r = requests.get(url)
-data = r.json()
+# define API key and base URL
+api_key = '02KCC7JFN4DFFWMH'
+base_url = 'https://www.alphavantage.co/query?function='
 
-print(data)
+# define function to request stock data
+def get_stock_data(symbol, interval):
+  url = base_url + 'TIME_SERIES_' + interval + '_ADJUSTED&symbol=' + symbol + '&apikey=' + api_key
+  response = requests.get(url)
+  data = response.json()
+  df = pd.DataFrame.from_dict(data['Time Series (Daily)'], orient='index')
+  df.index = pd.to_datetime(df.index)
+  df.columns = ['Open', 'High', 'Low', 'Close', 'Adjusted Close', 'Volume', 'Dividend', 'Split Coefficient']
+  df = df.astype(float)
+  return df
 
-# test
+# fetch stock data for a sample symbol and interval
+symbol = 'AAPL'
+interval = 'DAILY'
+data = get_stock_data(symbol, interval)
+
+# calculate daily returns
+data['Return'] = (data['Close'] / data['Close'].shift(1)) - 1
+
+# calculate 50-day moving average
+data['MA50'] = data['Close'].rolling(window=50).mean()
+
+# print the data
+print(data.head())
